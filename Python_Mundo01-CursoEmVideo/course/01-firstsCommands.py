1311c6f3a490bba4c007a8199413c502a108ebd6
Haroldo-Mayworm <haroldomayworm@gmail.com>
2024-04-18 17:34:58 -0300

    Feat: Basics commands in Python

---
+++ Python_Mundo01-CursoEmVideo/course/01-firstsCommands.py
@@ -0,0 +1,16 @@
+"""
+# print() - This function is used to display something on the screen
+print('My first command')
+print(5 + 4)
+
+# Declaring variables
+name = 'Haroldo'
+age = 22
+grade = 9.5
+print(name, age, grade)
+"""
+# input() - This function reads data from the terminal
+nameInput = input('What\'s your name? ')
+ageInput = input('How old are you? ')
+gradeInput = input('What was your grade? ')
+print(nameInput, ageInput, gradeInput)
