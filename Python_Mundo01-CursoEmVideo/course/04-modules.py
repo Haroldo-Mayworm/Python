56d26b23ce002def31134b49aa4fa6c1c414be4c
Haroldo-Mayworm <haroldomayworm@gmail.com>
2024-04-18 17:38:22 -0300

    Feat: Intro modules in Python

---
+++ Python_Mundo01-CursoEmVideo/course/04-modules.py
@@ -0,0 +1,11 @@
+"""
+import math
+# from math import sqrt
+numberInput = int(input('Enter a number: '))
+squareRoot = math.sqrt(numberInput)
+# squareRoot = sqrt(numberInput)
+print(f'The square root of the number {numberInput} is equal to {squareRoot:.5f}!')
+"""
+import random
+number = random.randint(1, 50)
+print(number)
