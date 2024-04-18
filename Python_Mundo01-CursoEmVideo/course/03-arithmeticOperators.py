c029ad784848820a49fe0dcff45ed6d5e4c900c4
Haroldo-Mayworm <haroldomayworm@gmail.com>
2024-04-18 17:37:45 -0300

    Feat: Arithmetic operators

---
+++ Python_Mundo01-CursoEmVideo/course/03-arithmeticOperators.py
@@ -0,0 +1,14 @@
+"""
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 ** 2)
+print(5 // 2)
+print(5 % 2)
+"""
+"""
+print(20 + 9 / 3)
+print(10 * 2 ** 3)
+print(6 * (10 + 8) ** 2)
+"""
