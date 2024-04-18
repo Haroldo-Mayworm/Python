c119064d1a2aa3deda56ce299bbabd93f828e11e
Haroldo-Mayworm <haroldomayworm@gmail.com>
2024-04-18 17:36:38 -0300

    Feat: Primitive data

---
+++ Python_Mundo01-CursoEmVideo/course/02-primitiveData.py
@@ -0,0 +1,21 @@
+"""
+numberInput = input('Enter a number: ')
+print(type(numberInput))
+print(type(int(numberInput)))
+
+numberInput01 = int(input('Enter a number: '))
+numberInput02 = int(input('Enter another number: '))
+numbersSum = numberInput01 + numberInput02
+print(numbersSum)
+
+numberInput01 = float(input('Enter a number: '))
+numberInput02 = float(input('Enter another number: '))
+numbersSum = numberInput01 + numberInput02
+print(numbersSum)
+
+numberInput01 = int(input('Enter a number: '))
+numberInput02 = float(input('Enter another number: '))
+numbersSum = numberInput01 + numberInput02
+print(numbersSum)
+print('The sum between {} and {} is worth {}'.format(numberInput01,numberInput02,numbersSum)
+"""
